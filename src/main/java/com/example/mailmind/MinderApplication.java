6f1a6e340b1d877127f976993877331b8c39dcf6
Malavika1908 <137694292+Malavika1908@users.noreply.github.com>
2025-05-28 13:51:39 +0530

    Add files via upload

---
+++ src/main/java/com/example/mailmind/MinderApplication.java
@@ -0,0 +1,14 @@
+package com.example.minder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling; // ADD THIS IMPORT
+
+@SpringBootApplication
+@EnableScheduling // ADD THIS ANNOTATION
+public class MinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinderApplication.class, args);
+    }
+}
